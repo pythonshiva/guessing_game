36c33e1f7d026580818ff664d1d3fc8998129c5e
Siva <Siva@Mulagalas-MacBook-Pro.local>
2018-11-17 14:06:52 +0530

    Guessing number

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Input your guess");
+
+    let mut guess = String::new();
+	let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Something has gone wrong");
+    println!("you have entered: {}", guess);
 }
