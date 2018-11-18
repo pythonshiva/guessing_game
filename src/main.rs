e8bf9afe42f04fa8ce4afcfa3b59de11e829c3c1
Siva <Siva@Mulagalas-MacBook-Pro.local>
2018-11-18 14:54:33 +0530

    Handling invalid input

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Input your guess");
+    let secret_num = rand::thread_rng().gen_range(1,101);
+    // println!("The secret number is: {}", secret_num);
+	loop {
+		println!("Input your guess");
+		let mut guess = String::new();
+		io::stdin().read_line(&mut guess).expect("Something has gone wrong");
+	    let guess: u32 = match guess.trim().parse() {
+	    	Ok(num) => num,
+	    	Err(_) => 	{
+	    		println!("Enter a numaric value");
+	    		continue;
+		    	}
+	    };
+	    println!("you have entered: {}", guess);
 
-    let mut guess = String::new();
-	let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Something has gone wrong");
-    println!("you have entered: {}", guess);
+	    match guess.cmp(&secret_num) {
+	    	Ordering::Less => println!("Too small"),
+	    	Ordering::Greater => println!("Too big"),
+	    	Ordering::Equal => {
+	    		println!("Congrats, You have guessed correct number!");
+	    		break;
+	    	}
+	    }
+	}
+    
 }
